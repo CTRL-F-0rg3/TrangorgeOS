2290cfa06249c0ffd345336fda4986a5342f8fa1
ctrl-f-0-rg3 <ctrlf0rge@protonmail.com>
2026-08-09 17:21:28 +0200

    fix and tested os

--- kernel/src/vga_buffer.rs
+++ kernel/src/vga_buffer.rs
@@ -1,9 +1,7 @@
-
 use volatile::Volatile;
 
-
 #[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq )]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
     Black = 0,
@@ -20,8 +18,8 @@ pub enum Color {
     LightCyan = 11,
     LightRed = 12,
     Pink = 13,
-    Yellow = 14
-    White = 15
+    Yellow = 14,
+    White = 15,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -30,7 +28,7 @@ struct ColorCode(u8);
 
 impl ColorCode {
     fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8)<< 4 | (foreground as u8))
+        ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
 
@@ -38,20 +36,19 @@ impl ColorCode {
 #[repr(C)]
 struct ScreenChar {
     ascii_character: u8,
-    color_code : ColorCode,
+    color_code: ColorCode,
 }
 
-const BUFFER_HEIGHT: unsize = 25;
-const BUFFER_WIDTH: unsize = 80;
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT], //useless 
-    char: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
-    column_position: unsize,
+    column_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
@@ -63,28 +60,47 @@ impl Writer {
             byte => {
                 if self.column_position >= BUFFER_WIDTH {
                     self.new_line();
-                
                 }
 
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
 
                 self.column_position += 1;
             }
         }
     }
-    fn new_line(&mut self) {/* TODO */}
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
-                0x20.. = 0x7e | b'\n' => self.write_byte(byte),
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
                 _ => self.write_byte(0xfe),
             }
         }
@@ -95,13 +111,9 @@ pub fn print_something() {
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe {
-            &mut *(0xbb8000 as *mut Buffer) 
-        },
-
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     };
     writer.write_byte(b'H');
     writer.write_string("ello ");
     writer.write_string("world ");
-
 }
