fdde5c1aad0a42c00dac4808b5d3721714f1896b
CTRL-F-0rg3 <tdddet574@gmail.com>
2026-08-12 17:16:26 +0200

    limine

--- comlimine/src/main.c
+++ comlimine/src/main.c
@@ -1,15 +1,11 @@
-
+// comlimine/src/main.c
 #include <stdint.h>
 #include <stddef.h>
 #include "limine.h"
 
 extern void kernel_main(uint64_t magic, void *info);
 
-static volatile struct limine_bootloader_info_request bootloader_info = {
-    .id = LIMINE_BOOTLOADER_INFO_REQUEST,
-    .revision = 0,
-    .response = NULL,
-};
+LIMINE_BASE_REVISION(1);
 
 static volatile struct limine_framebuffer_request framebuffer = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
@@ -29,27 +25,14 @@ static volatile struct limine_hhdm_request hhdm = {
     .response = NULL,
 };
 
-static volatile struct limine_kernel_address_request kernel_address = {
-    .id = LIMINE_KERNEL_ADDRESS_REQUEST,
-    .revision = 0,
-    .response = NULL,
-};
-
 void _start(void) {
-    if (bootloader_info.response == NULL ||
-        framebuffer.response == NULL ||
+    if (framebuffer.response == NULL ||
         memmap.response == NULL ||
-        hhdm.response == NULL ||
-        kernel_address.response == NULL) {
-        for (;;) {
-            asm volatile ("hlt");
-        }
+        hhdm.response == NULL) {
+        for (;;) { asm volatile ("hlt"); }
     }
 
     kernel_main(0, (void *)framebuffer.response);
 
-
-    for (;;) {
-        asm volatile ("hlt");
-    }
+    for (;;) { asm volatile ("hlt"); }
 }
