5c0c3c615012e85407a2d83ffb6aef9f6926f034
CTRL-F-0rg3 <tdddet574@gmail.com>
2026-08-19 18:49:51 +0200

    bt

--- kernel/src/bluetooth/operation.h
+++ kernel/src/bluetooth/operation.h
@@ -3,6 +3,11 @@
 
 #include "bt.h"
 
+#define EVT_Q   16
+#define EVT_MAX 64
+#define ACL_Q   8
+#define ACL_MAX 256
+
 void bt_op_model_init(void);
 void bt_op_model_run_cmd(uint16_t opcode, const uint8_t *params, uint8_t len);
 
