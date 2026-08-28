7d989d52fe92f3dc3c5b74002f8aa081fbc4a786
CTRL-F-0rg3 <tdddet574@gmail.com>
2026-08-28 14:45:39 +0200

    [panic] kernel panic fix

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -60,6 +60,7 @@ impl InterruptIndex {
 }
 extern "x86-interrupt" 
 fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    BREAKPOINT_HITS.fetch_add(1, Ordering::SeqCst);
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
