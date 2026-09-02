cf4ec9ad42d2ae7a85ebf18e1e19340d65c106b6
ctrl-f-0-rg3 <ctrlf0rge@protonmail.com>
2026-09-02 12:14:56 +0200

    bitmap and tracepoint

---
+++ kernel/src/cpu/shelduler/debug/tracepoints.rs
@@ -0,0 +1,41 @@
+pub struct Tracepoint {}
+
+pub struct TracePointId {}
+
+pub struct Tracepointkind{}
+
+pub struct tracepoints {
+    // za chwile
+}
+
+pub enum Tracepoint {
+
+}
+
+pub struct TraceEvent{
+
+}
+
+pub enum TraceData{
+
+}
+
+pub struct ContextSwichTraceData{
+
+}
+
+pub struct TraceContext{
+
+}
+
+pub struct TracepointRegistry{
+
+}
+
+pub struct RegisteredTracepoint{}
+
+pub struct TraceBuffer{}
+
+pub struct PerCpuTracebuffer{}
+
+// TODO uzupełnijpotem bo na ten moment nie ma czym 
