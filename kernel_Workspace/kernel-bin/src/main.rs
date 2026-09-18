777fb8f57631817ffa902f6e21d36e67d8f8b51b
ctrl-f-0-rg3 <ctrlf0rge@protonmail.com>
2026-09-18 11:22:15 +0200

    [kernel workspace] kernel libs

--- kernel_Workspace/kernel-bin/src/main.rs
+++ kernel_Workspace/kernel-bin/src/main.rs
@@ -1,2 +1,12 @@
-#[no_std]
-#[no_main]
+#![no_std]
+#![no_main]
+
+use bootloader::{entry_point, BootInfo};
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    kernel::init(boot_info);
+
+    loop {}
+}
