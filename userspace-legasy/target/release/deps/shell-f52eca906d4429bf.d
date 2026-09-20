515f1ecbf21189bb02da4063f69fb6c49593186d
CTRL-F-0rg3 <tdddet574@gmail.com>
2026-09-20 20:29:30 +0200

    gfx protocol gfx server vise lg driverspace register vgpu driver

--- userspace-legasy/target/release/deps/shell-f52eca906d4429bf.d
+++
@@ -1,5 +0,0 @@
-/home/ctrl/TrangorgeOS/userspace/target/release/deps/shell-f52eca906d4429bf.d: shell/src/main.rs
-
-/home/ctrl/TrangorgeOS/userspace/target/release/deps/shell-f52eca906d4429bf: shell/src/main.rs
-
-shell/src/main.rs:
